0b2a773a30ba0b3e109972b44ca0b047ca495e21
Adam Greene <copyright@mzpqnxow.com>
2020-05-02 12:12:53 -0400

    Commit of project specific files

---
+++ bin/start_tftp.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+CURDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+SUDO=sudo
+# Uncomment the next line if you are already running this script as root
+# SUDO=
+PYTHON=python3
+TFTP_SERVER="tftpy_server.py"
+TFTP_ROOT="${CURDIR}/serv/tftproot"
+FAKE_SUCCESS=/bin/true
+USERNAME="$(whoami)"
+VENV_BIN=${CURDIR}/venv/bin
+UMASK=000
+XTERM_TITLE='TFTP Server'
+######################################################################
+deactivate 2>&1 2>/dev/null || ${FAKE_SUCCESS}
+# echo Enter venv ...
+# source venv/bin/activate || (echo Building venv && make rebuild && source venv/bin/activate)
+# echo Starting tftp server ...
+nohup xterm -bg black -T "${XTERM_TITLE}" -fg green -geometry 0x60x20 -e ${SUDO} ${VENV_BIN}/${PYTHON} ${VENV_BIN}/${TFTP_SERVER} -r ${TFTP_ROOT} &
+if [ "$?" -eq "0" ]; then
+  echo Started successfully @ "${TFTP_ROOT}" || echo Failed to start
+else
+  echo Failed to start, check nohup log
+fi
+
