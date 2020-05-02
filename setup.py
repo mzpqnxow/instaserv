6f419627f89b5ed76387adcfbd32676aace3c676
Adam Greene <copyright@mzpqnxow.com>
2020-05-02 12:15:45 -0400

    Fix setuptools files

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ This is the setup.py for company.group.pypackagedir
 Perform tasks
 
 Includes a package, an application and some data files
+
+https://packaging.python.org/tutorials/packaging-projects/#initial-files
 """
 from glob import glob
 from os import walk
@@ -48,9 +50,6 @@ OU = 'group'
 NAMESPACE = [ORG, OU]
 NAME = '.'.join(NAMESPACE + [PROJECT_NAME])
 
-AUTHOR = 'John Q. Doe'
-DESCRIPTION = 'A package for doing various things'
-EMAIL = 'jqdoe@company.com'
 EXCLUDE_FILES = (
     '.keep',
     'constraints.txt',
@@ -62,33 +61,40 @@ LICENSE = 'Proprietary'
 PACKAGE_DATA_DIRS = ['etc/packagedir']
 PYTHON_REQUIRES = '>=3'
 
-# Keep this in sync with venv/requirements-project.txt for a nice development experience
-REQUIRED = ['package1', 'package2', 'package3']
+# Set the install_requires value in setup.cfg, you don't need to change it here
+# REQUIRED = ['package1', 'package2', 'package3']
 SCRIPTS = glob('bin/*')
-URL = 'https://www.company.com'
 
 DATA_FILE_LIST = enumerate_static_content(PACKAGE_DATA_DIRS)
+
+# ENTRY_POINTS = {
+#     'console_scripts': [
+#         'cli_app = app:main',
+#     ],
+# },
 
 # The below version uses versioneer, you'll need to remove `cmdclass` and change
 # `version` to a fixed string like `0.0.1` if you don't want to use versioneer
 # If you *do* want versioneer, you'll need to do `versioneer install` inside
 # the root of the project to bootstrap it, then make some changes to setup.cfg
 setup(
-    author=AUTHOR,
-    author_email=EMAIL,
     cmdclass=versioneer.get_cmdclass(),
-    data_files=DATA_FILE_LIST,
-    description=DESCRIPTION,
-    include_package_data=True,
-    install_requires=REQUIRED,
-    license=LICENSE,
-    name=NAME,
-    packages=find_packages(),
-    python_requires=PYTHON_REQUIRES,
-    scripts=SCRIPTS,
-    url=URL,
     version=versioneer.get_version(),
-    zip_safe=False)
+    data_files=DATA_FILE_LIST,
+    # The following can all be set in setup.cfg:
+    # include_package_data=True,
+    # install_requires=REQUIRED,
+    # license=LICENSE,
+    # name=NAME,
+    # Use setup.cfg:
+    # The line:
+    # packages = find:
+    # Is equivalent to the argument:
+    # packages=find_packages(),
+    # Use python_requires in setup.cfg instead
+    # python_requires=PYTHON_REQUIRES,
+    scripts=SCRIPTS
+)
 
 
 #
