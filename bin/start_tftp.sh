1c1fc84e9f4c1274c3fd2f7c06ae3a26437879cb
Adam Greene <copyright@mzpqnxow.com>
2020-05-02 12:30:21 -0400

    Change start_tftp to work when installed or when in a checked out repo (dev mode)

--- bin/start_tftp.sh
+++ bin/start_tftp.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 CURDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 SUDO=sudo
-# Uncomment the next line if you are already running this script as root
 # SUDO=
-PYTHON=python3
+# Development mode, running directory from git repo root
+if [ -z ${VIRTUAL_ENV+x} ]; then
+    source ../venv/bin/activate
+fi
+# echo Enter venv ...
+# source venv/bin/activate || (echo Building venv && make rebuild && source venv/bin/activate)
+# echo Starting tftp server ...
+
+declare -r VENV_ROOT="${VIRTUAL_ENV}"
+PYTHON=$(command -v python3)
 TFTP_SERVER="tftpy_server.py"
-TFTP_ROOT="${CURDIR}/serv/tftproot"
+TFTP_ROOT="${VENV_ROOT}/serv/tftproot"
 FAKE_SUCCESS=/bin/true
 USERNAME="$(whoami)"
-VENV_BIN=${CURDIR}/venv/bin
+VENV_BIN=${VENV_ROOT}/bin
 UMASK=000
 XTERM_TITLE='TFTP Server'
 ######################################################################
 deactivate 2>&1 2>/dev/null || ${FAKE_SUCCESS}
-# echo Enter venv ...
-# source venv/bin/activate || (echo Building venv && make rebuild && source venv/bin/activate)
-# echo Starting tftp server ...
-nohup xterm -bg black -T "${XTERM_TITLE}" -fg green -geometry 0x60x20 -e ${SUDO} ${VENV_BIN}/${PYTHON} ${VENV_BIN}/${TFTP_SERVER} -r ${TFTP_ROOT} &
+nohup xterm -bg black -T "${XTERM_TITLE}" -fg green -geometry 0x60x20 -e ${SUDO} ${TFTP_SERVER} -r ${TFTP_ROOT} &
 if [ "$?" -eq "0" ]; then
   echo Started successfully @ "${TFTP_ROOT}" || echo Failed to start
 else
